8e2f5c6d3256190f3724b41132b0514280e5a5de
rootBotNet <wallacevanzyl@outlook.com>
2019-08-04 14:57:44 +0200

    Add more pre loaded data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 INSERT INTO person_details
 	( name, age, ageCategory )
 VALUES
-	( 'Samuel', '25', 'Middle' );
+	( 'Samuel', '25', 'Middle' ),
+	( 'Jacob', '48', 'Old' ),
+	( 'Isaac', '69', 'Old' ),
+	( 'David', '12', 'Young' ),
+	( 'John', '20', 'Young' ),
+	( 'Rebekah', '67', 'Old' ),
+	( 'Mary', '78', 'Old' ),
+	( 'Joseph', '38', 'Middle' ),
+	( 'Paul', '55', 'Old' ),
+	( 'Peter', '47', 'Old' );
