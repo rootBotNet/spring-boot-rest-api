b5c0839323e80a01b48a25782294125082936dea
f5267439 <wallace.vanzyl@fnb.co.za>
2019-08-06 12:17:16 +0200

    Add tests for rest controller layer.

--- src/test/java/za/co/wvz/person/details/v1/PersonDetailsRestControllerIntegrationTest.java
+++ src/test/java/za/co/wvz/person/details/v1/PersonDetailsRestControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import za.co.wvz.person.details.v1.controller.PersonDetailsController;
@@ -16,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,26 +42,57 @@ public class PersonDetailsRestControllerIntegrationTest {
     @MockBean
     CalculateAgeCategory calculateAgeCategory;
 
-    @WithMockUser(value = "admin")
     @Test
-    public void givenPersonDetails_whenGetPersonDetails_thenReturnJsonArray() throws Exception{
+    public void givenPersonDetails_whenGetAllPersonDetails_thenReturnJsonArray_Admin_User() throws Exception{
         List<PersonDetails> personDetailsList;
         personDetailsList = createPersonDetails();
 
         given(personDetailsService.findAll()).willReturn(personDetailsList);
 
         mvc.perform(get("/v1/person")
+                .with(httpBasic("admin", "p@55w0rd"))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(personDetailsList.get(0).getId())))
                 .andExpect(jsonPath("$[0].name", is(personDetailsList.get(0).getName())))
                 .andExpect(jsonPath("$[0].age", is(personDetailsList.get(0).getAge())))
-                .andExpect(jsonPath("$[0].name", is(personDetailsList.get(0).getAgeCategory())))
-                .andExpect(jsonPath("$[1].id", is(personDetailsList.get(1).getId())))
+                .andExpect(jsonPath("$[0].ageCategory", is(personDetailsList.get(0).getAgeCategory())))
                 .andExpect(jsonPath("$[1].name", is(personDetailsList.get(1).getName())))
                 .andExpect(jsonPath("$[1].age", is(personDetailsList.get(1).getAge())))
-                .andExpect(jsonPath("$[1].name", is(personDetailsList.get(1).getAgeCategory())));
+                .andExpect(jsonPath("$[1].ageCategory", is(personDetailsList.get(1).getAgeCategory())));
+    }
+
+    @Test
+    public void givenPersonDetails_whenGetAllPersonDetails_thenReturnJsonArray_Guest_User() throws Exception{
+        List<PersonDetails> personDetailsList;
+        personDetailsList = createPersonDetails();
+
+        given(personDetailsService.findAll()).willReturn(personDetailsList);
+
+        mvc.perform(get("/v1/person")
+                .with(httpBasic("guest", "p@55w0rd1"))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is(personDetailsList.get(0).getName())))
+                .andExpect(jsonPath("$[0].age", is(personDetailsList.get(0).getAge())))
+                .andExpect(jsonPath("$[0].ageCategory", is(personDetailsList.get(0).getAgeCategory())))
+                .andExpect(jsonPath("$[1].name", is(personDetailsList.get(1).getName())))
+                .andExpect(jsonPath("$[1].age", is(personDetailsList.get(1).getAge())))
+                .andExpect(jsonPath("$[1].ageCategory", is(personDetailsList.get(1).getAgeCategory())));
+    }
+
+    @Test
+    public void givenPersonDetails_whenGetAllPersonDetails_thenReturnJsonArray_Forbidden_User() throws Exception{
+        List<PersonDetails> personDetailsList;
+        personDetailsList = createPersonDetails();
+
+        given(personDetailsService.findAll()).willReturn(personDetailsList);
+
+        mvc.perform(get("/v1/person")
+                .with(httpBasic("user", "unauthorized"))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
     }
 
     private List<PersonDetails> createPersonDetails(){
